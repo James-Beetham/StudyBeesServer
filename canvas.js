2df6ff97fbc1ca3f3a4fe1af8ba0133ec0af9fc4
fkatleader2 <fkatleader3@gmail.com>
2018-09-08 19:32:29 -0400

    Implemented canvas and editor

--- canvas.js
+++ canvas.js
@@ -1,9 +1,16 @@
 /**
- * socket listeners (on): 
- * socket executors (emit): 
+ * socket listeners (on): sendCanvas
+ * socket executors (emit): addCanvas, error
  */
 module.exports = {
     init: function(socket, d) { // socket is this user's socket, d is server data
-
+        socket.on('sendCanvas', (data) => {
+            if (typeof data !== 'string') { socket.emit('err', {type: 'canvas', msg: 'invalid data type: ' + (typeof data)}); return; }
+            if (d.users[socket.id].session === undefined) { socket.emit('err', {type: 'canvas', msg: 'user not in session'}); return; }
+            if (d.users[socket.id].course == null) { socket.emit('err', {type: 'canvas', msg: 'user data missing'}); return; }
+            var entry = {time: (new Date().getTime()), data: data, name: d.users[socket.id].data.name};
+            d.users[socket.id].session.canvas.history.push(entry);
+            d.users[d.users[socket.id].partner].socket.emit('addCanvas', entry);
+        });
     }
 };
--- editor.js
+++ editor.js
@@ -1,9 +1,16 @@
 /**
- * socket listeners (on): 
- * socket executors (emit): 
+ * socket listeners (on): sendEditor
+ * socket executors (emit): addEditor, error
  */
 module.exports = {
     init: function(socket, d) { // socket is this user's socket, d is server data
-
+        socket.on('sendEditor', (data) => {
+            if (typeof data !== 'string') { socket.emit('err', {type: 'editor', msg: 'invalid data type: ' + (typeof data)}); return; }
+            if (d.users[socket.id].session === undefined) { socket.emit('err', {type: 'editor', msg: 'user not in session'}); return; }
+            if (d.users[socket.id].course == null) { socket.emit('err', {type: 'editor', msg: 'user data missing'}); return; }
+            var entry = {time: (new Date().getTime()), data: data, name: d.users[socket.id].data.name};
+            d.users[socket.id].session.editor.history.push(entry);
+            d.users[d.users[socket.id].partner].socket.emit('addEditor', entry);
+        });
     }
 };
