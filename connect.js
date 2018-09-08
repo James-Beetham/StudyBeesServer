817fcfe74ca6fe396a769d6b072a3fc6008fbe90
fkatleader2 <fkatleader3@gmail.com>
2018-09-08 13:16:29 -0400

    bug fix

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 module.exports = {
     init: function(socket, d) { // socket is this user's socket, d is server data
-        socket.on('connect', (data) {
+        socket.on('connect', (data) => {
             console.log(socket.id + ' sent connect request');
         });
     }
