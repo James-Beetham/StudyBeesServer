5716c2c43a19a05e54b4fbe8efe3a33e5421f631
fkatleader2 <fkatleader3@gmail.com>
2018-09-08 22:50:33 -0400

    generalized task data

--- tasks.js
+++ tasks.js
@@ -5,11 +5,10 @@
 module.exports = {
     init: function(socket, d) { // socket is this user's socket, d is server data
         socket.on('sendTask', (data) => {
-            if (typeof data !== 'string') { socket.emit('err', {type: 'tasks', msg: 'invalid data type: ' + (typeof data)}); return; }
             if (d.users[socket.id].session === undefined) { socket.emit('err', {type: 'tasks', msg: 'user not in session'}); return; }
             if (d.users[socket.id].course == null) { socket.emit('err', {type: 'tasks', msg: 'user data missing'}); return; }
 
-            var entry = {time: (new Date().getTime()), task: data};
+            var entry = {time: (new Date().getTime()), data: data};
             d.users[socket.id].session.tasks.history.push(entry);
             d.users[d.users[socket.id].partner].socket.emit('addTask', entry);
         });
