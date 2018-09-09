d3e664eac45eff41ded02833c9292a35fff730ed
fkatleader2 <fkatleader3@gmail.com>
2018-09-08 22:46:28 -0400

    fixed task bug

--- tasks.js
+++ tasks.js
@@ -4,14 +4,14 @@
  */
 module.exports = {
     init: function(socket, d) { // socket is this user's socket, d is server data
-        socket.on('addTask', (data) => {
+        socket.on('sendTask', (data) => {
             if (typeof data !== 'string') { socket.emit('err', {type: 'tasks', msg: 'invalid data type: ' + (typeof data)}); return; }
             if (d.users[socket.id].session === undefined) { socket.emit('err', {type: 'tasks', msg: 'user not in session'}); return; }
             if (d.users[socket.id].course == null) { socket.emit('err', {type: 'tasks', msg: 'user data missing'}); return; }
 
             var entry = {time: (new Date().getTime()), task: data};
             d.users[socket.id].session.tasks.history.push(entry);
-            d.users[d.users[socket.id].partner].socket.emit('addMessage', entry);
+            d.users[d.users[socket.id].partner].socket.emit('addTask', entry);
         });
     }
 };
